1160f709dc93ede3e57979bfdab467e7118ae8f0
Mucha <JunichiMuto@users.noreply.github.com>
2017-03-12 23:51:32 +0900

    コメント追加

--- Java8-Study/src/RandomText.java
+++ Java8-Study/src/RandomText.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.IntFunction;
 import java.util.function.IntPredicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -12,14 +13,6 @@ public class RandomText {
 	public static interface IntTernaryPredicate{
 		boolean is(int a, int b, int c);
 	}
-	public static void main(String[] args) throws InterruptedException {
-
-//		List<String> delimiters = Arrays.asList(" ","\r","\n","\t");
-		List<String> delimiters = Arrays.asList(" ");
-
-		String result = randomText(10_000, 3, 8, delimiters);
-		System.out.println(result);
-	}
 
 	public static String randomText(int words, int lower, int upper, List<String> delimiters){
 
@@ -28,7 +21,7 @@ public class RandomText {
 		};
 
 		//0x20はスペース、21～2Fは記号
-		IntPredicate symbol = i -> withinRange.is(i, 0x20, 0x2f);
+//		IntPredicate symbol = i -> withinRange.is(i, 0x20, 0x2f);
 		//alphabetと数字 0x5cはバックスラッシュなので除く
 		IntPredicate alnum = i -> withinRange.is(i, 0x30, 0x7e)  && i != 0x5c;
 		//ひらがな
@@ -38,39 +31,69 @@ public class RandomText {
 		//半角カナ
 		IntPredicate hkana = i -> withinRange.is(i, 0xff66, 0xff9f);
 
+		//連結して1つのIntPredicateを得る
 		IntPredicate acceptLetter = alnum.or(hiragana).or(katakana).or(hkana);
 
+		//使用する文字のリストを得る
 		List<String> chars = IntStream.rangeClosed(0x1, 0xff9f)
 				.filter(acceptLetter)
+				//Unicodeコードポイント(int)→UTF-16文字(char[])→String
+				//String.valueOf(i)としてしまうと数値がそのまま文字になるだけなので✕
 			    .mapToObj(i -> String.valueOf(Character.toChars(i)))
 			    .collect(Collectors.toList());
 
+		//区切り文字をランダムに取得する
 		Supplier<String> delm;
 		if(delimiters.size() == 1){
+			//要素が一つの場合はそれを返すだけのラムダ式に
 			String d = delimiters.get(0);
 			delm = () -> d;
 		}else{
-			delm = () -> delimiters.get(ThreadLocalRandom.current().nextInt(delimiters.size()));
+			//複数あったら乱数で
+			delm = () -> delimiters.get(
+					ThreadLocalRandom.current().nextInt(delimiters.size()));
 		}
 
-		String result = ThreadLocalRandom.current().ints(words, lower, upper)
-			.parallel()
-			.mapToObj(len ->
-				ThreadLocalRandom.current().ints(0, chars.size())
-					.limit(len)
-					.mapToObj(chars::get)
-					.collect(Collectors.joining())
-			)
-			.collect(() -> new StringBuilder(words * upper / 10),
-					(sb, str) -> sb.append(delm.get()).append(str),
-					StringBuilder::append)
-			.substring(1);
+		//引数の文字数でランダムな文字列を生成するラムダ式
+		IntFunction<String> getWord = i ->
+			//リストのインデックスの範囲で乱数生成
+			ThreadLocalRandom.current().ints(0, chars.size())
+				//渡された長さまで
+				.limit(i)
+				//インデックスから文字を取り出す
+				.mapToObj(chars::get)
+				//１つの文字列へ結合
+				.collect(Collectors.joining());
+
+		//文字数下限～上限の範囲で乱数生成
+		String result = ThreadLocalRandom.current().ints(lower, upper)
+				.parallel()
+				//渡された単語数分まで
+				.limit(words)
+				//ランダムな単語を得る
+				.mapToObj(getWord)
+				//結合 Collectors#joining(String)だと区切り文字が１つしか指定できないので3つ指定する方
+				//supplierはStringBuilderのコンストラクタ
+				.collect(StringBuilder::new,
+						//accumulatorは区切り文字を追加して結合
+						(sb, str) -> sb.append(delm.get()).append(str),
+						//combinerはStringBuilderの場合appendでコンテナ同士を結合できる
+						StringBuilder::append)
+				//上記だと一番先頭にも区切り文字が入ってしまうため、toStringではなくsubstring(1)で除去しつつStringへ
+				.substring(1);
 
 		return result;
 
 	}
 
-	public static boolean withinRange(int val, int lower, int upper){
-		return lower <= val && val <= upper;
+	public static void main(String[] args) {
+
+//		List<String> delimiters = Arrays.asList(" ","\r","\n","\t");
+		List<String> delimiters = Arrays.asList(" ");
+
+		String result = randomText(10_000, 3, 8, delimiters);
+		System.out.println(result);
 	}
+
+
 }
